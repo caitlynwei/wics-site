6ae281e932f633c54cd98bbe34694059c918e05c
caitlynwei <caitlyn.wei@tufts.edu>
2025-11-11 12:26:59 -0500

    test

--- app/sprint2/page.tsx
+++ app/sprint2/page.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import { Container, Title, Text, Stack, Button } from "@mantine/core";
+// Add route to ticket component here from your file
 
 export default function Sprint2() {
   return (
     <Container size="md" py={48}>
       <Stack>
         <Title order={2}>Sprint 2</Title>
-        <Text>Put your ticket here</Text>
+        <Text>Put your ticket here!</Text>
         {/* Ticket Component */}
         <Link href="/">
           <Button>Back to Sprint Page</Button>
